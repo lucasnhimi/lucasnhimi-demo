f4adccd6ebe61972f05eead3cb74378879c86900
Lucas Nhimi <lucasnhimi@gmail.com>
2021-01-22 14:56:10 -0300

    Step 2 - Configure Chakra-UI and Absolute Paths

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,18 @@
+import { Box, Button, Text } from '@chakra-ui/react';
 import useAuth from './../hooks/useAuth';
 
 function Dashboard(props) {
   const {user} = useAuth();
   console.log(user)
-  
+
   return (
-    <div>
-      {user?.displayName}
-    </div>
-  );
+    <Box>
+      <Box mb={2}>
+        <Text as="span" fontWeight="bold" display="inline">user: {user?.displayName}</Text>
+      </Box>
+      <Button variant="outline" colorScheme="blue"  onClick={() => signin()}>Sair</Button>        
+    </Box>
+  ); 
 }
 
 export default Dashboard;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
-import Head from 'next/head'
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+
 import useAuth from './../hooks/useAuth';
 
 export default function Home() {
   const { signin, user } = useAuth();
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <span>user: {user?.name}</span>
-        <button onClick={() => signin()}>Entrar com github</button>
-      </main>    
-    </div>
+  return (
+    <Box bg="gray.100" p={4} h="100%">
+      <Flex as="main" direction="column" maxW="700px" margin="0 auto">
+        <main>          
+          <Button variant="outline" colorScheme="blue"  onClick={() => signin()}>Entrar com github</Button>        
+        </main>    
+      </Flex>
+    </Box>
   )
 }
