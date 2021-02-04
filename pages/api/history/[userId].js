057e8570cd60b99b7f6c320a3eb7c53521d168d9
Lucas Nhimi <lucasnhimi@gmail.com>
2021-02-04 19:50:05 -0300

    Deploy

--- pages/api/history/[userId].js
+++ pages/api/history/[userId].js
@@ -3,6 +3,7 @@ import { getUserHistory } from '@/lib/db-admin';
 export default async (req, res) => {
   try {
     const { userId } = req.query;
+    console.log(userId);
     const { history } = await getUserHistory(userId);
 
     res.status(200).json({ history });
